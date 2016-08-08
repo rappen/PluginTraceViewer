44c0e3ee5e228fc21f1117993c2f45a5492f5cba
Jonas Rapp <jonas.rapp@cinteros.se>
2016-08-08 17:46:22 +0200

    Display name now also promoted to BETA

--- Cinteros.XTB.PluginTraceViewer/PluginDescription.cs
+++ Cinteros.XTB.PluginTraceViewer/PluginDescription.cs
@@ -6,7 +6,7 @@
     using XrmToolBox.Extensibility.Interfaces;
 
     [Export(typeof(IXrmToolBoxPlugin)),
-    ExportMetadata("Name", "Plugin Trace Viewer ALPHA"),
+    ExportMetadata("Name", "Plugin Trace Viewer BETA"),
     ExportMetadata("Description", "Investigate the Plug-in Trace Log"),
     ExportMetadata("SmallImageBase64", Constants.B64_IMAGE_SMALL), 
     ExportMetadata("BigImageBase64", Constants.B64_IMAGE_LARGE), 
