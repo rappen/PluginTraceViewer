bdb51bb7a209cd7c01423532b550ed2ee55bfcc6
Jonas Rapp <jonas.rapp@innofactor.com>
2018-06-12 08:41:39 +0200

    Updated plugin description

--- Cinteros.XTB.PluginTraceViewer/Properties/AssemblyInfo.cs
+++ Cinteros.XTB.PluginTraceViewer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Plug-in Trace Viewer")]
+[assembly: AssemblyTitle("Plugin Trace Viewer")]
 [assembly: AssemblyDescription("XrmToolBox plugin to inspect and filter the Plug-in Trace Log")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Jonas Rapp, Innofactor Sweden")]
+[assembly: AssemblyCompany("Jonas Rapp, Sweden")]
 [assembly: AssemblyProduct("Cinteros.XTB.PluginTraceViewer")]
-[assembly: AssemblyCopyright("Copyright © Innofactor AB 2018")]
+[assembly: AssemblyCopyright("Copyright © Jonas Rapp 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
